64f7cdfb9234575bd0b2f3b5402e3454d520b821
steve <stevedanko1@gmail.com>
2013-12-02 17:57:15 -0500

    added in a loop so you can keeyp adding houses until you enter one wt data

--- get_my_mortgage.rb
+++ get_my_mortgage.rb
@@ -7,16 +7,23 @@ homes.load_houses('mortgage_prices.csv')
 puts 'What is the address of the home you want to get insurence information for?'
 address = gets.chomp
 
-house = HomePurchaseOption.new(homes, address)
-
+while true
+  house = HomePurchaseOption.new(homes, address)
+  if house.check_for_adress(address)
+    house.display_address
+    house.required_mortgage
+    house.pmi_required?
+    break
+  end
+  if house.check_for_adress(address) == false
+    puts 'No homes for that address'
+    address = gets.chomp
+  end
+  break if address.downcase == 'done'
+end
 if house.check_for_adress(address)
-  house.display_address
-  house.required_mortgage
-  house.pmi_required?
-
   puts "Do you want a quote on your insurence?"
   answer =gets.chomp
-
     if answer.downcase == 'yes'
       puts 'How many years'
       years = gets.chomp.to_i
